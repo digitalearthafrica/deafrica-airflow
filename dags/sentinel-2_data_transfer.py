08556d276bbcba523f6c4283fed982c686d5a103
Toktam <toktam.ebadi@ga.gov.au>
2020-06-12 16:52:18 +1000

    Run DAG every 5 minutes.

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -67,7 +67,7 @@ def copy_s3_objects(ti, **kwargs):
                                 dest_bucket_name=default_args['dest_bucket_name'])
 
 with DAG('migrate_s3_to_s3', default_args=default_args,
-         schedule_interval="@once",
+         schedule_interval=default_args['schedule_interval'],
          catchup=False, dagrun_timeout=timedelta(seconds=60)) as dag:
 
     process_sqs = SQSSensor(
