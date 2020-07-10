48d5d3f40caecb4534254697b287fb2d3c263622
Toktam <toktam.ebadi@ga.gov.au>
2020-07-10 15:46:47 +1000

    Read 500 message every 8 hours.

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -129,8 +129,8 @@ def trigger_sensor(ti, **kwargs):
     queue = get_queue()
     print("Queue size:", int(queue.attributes.get("ApproximateNumberOfMessages")))
     if int(queue.attributes.get("ApproximateNumberOfMessages")) > 0 :
-        max_num_polls = 1
-        msg_list = [queue.receive_messages(WaitTimeSeconds=5, MaxNumberOfMessages=2) for i in range(max_num_polls)]
+        max_num_polls = 50
+        msg_list = [queue.receive_messages(WaitTimeSeconds=5, MaxNumberOfMessages=10) for i in range(max_num_polls)]
         msg_list  = list(itertools.chain(*msg_list))
         messages = []
         for msg in msg_list:
