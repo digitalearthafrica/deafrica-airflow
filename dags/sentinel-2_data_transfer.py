12c1e15b250cf4f297b78dcca5448f7a6972217e
Toktam <toktam.ebadi@ga.gov.au>
2020-07-14 16:08:56 +1000

    Added missing import.

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -19,6 +19,8 @@ from airflow import configuration
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from airflow.contrib.sensors.aws_sqs_sensor import SQSHook
+from airflow.contrib.hooks.aws_sns_hook import AWS
+
 from airflow.hooks.S3_hook import S3Hook
 
 default_args = {
