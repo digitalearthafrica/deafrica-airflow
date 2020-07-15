c5e295fd9970beb0f1b0089169df51e5660949c0
Toktam <toktam.ebadi@ga.gov.au>
2020-07-15 16:24:52 +1000

    Moving ARN string to one line.

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -36,8 +36,7 @@ default_args = {
     "dest_bucket_name": "deafrica-sentinel-2",
     "src_bucket_name": "sentinel-cogs",
     "schedule_interval": "0 */8 * * *",
-    "sentinel2_topic_arn": ("arn:aws:sns:af-south-1:543785577597:"
-                           "deafrica-sentinel-2-scene-topic"),
+    "sentinel2_topic_arn": ("arn:aws:sns:af-south-1:543785577597:deafrica-sentinel-2-scene-topic"),
     "sqs_queue": ("https://sqs.us-west-2.amazonaws.com/565417506782/"
                   "deafrica-prod-eks-sentinel-2-data-transfer")
 }
