756db46f066371672f0982d7b16441b853813698
Toktam <toktam.ebadi@ga.gov.au>
2020-08-28 16:57:03 +1000

    Increase run frequency to every 6 hours.

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -35,7 +35,7 @@ default_args = {
     "us_conn_id": "deafrica-migration_us",
     "dest_bucket_name": "deafrica-sentinel-2",
     "src_bucket_name": "sentinel-cogs",
-    "schedule_interval": "0 */8 * * *",
+    "schedule_interval": "0 */6 * * *",
     "sentinel2_topic_arn": "arn:aws:sns:af-south-1:543785577597:deafrica-sentinel-2-scene-topic",
     "sqs_queue": ("https://sqs.us-west-2.amazonaws.com/565417506782/"
                   "deafrica-prod-eks-sentinel-2-data-transfer")
