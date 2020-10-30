529714423e595a9feb4b62123de2f9bb2321ee05
tebadi <toktam.ebadi@ga.gov.au>
2020-10-30 15:18:33 +1100

    Correct stac info before it's sent to the indexing topic

--- dags/sentinel-2_data_transfer.py
+++ dags/sentinel-2_data_transfer.py
@@ -77,9 +77,11 @@ def publish_to_sns_topic(message, attribute):
     """
 
     sns_hook = AwsSnsHook(aws_conn_id=dag.default_args['africa_conn_id'])
+
     "Correct region and bucket name before publishing to indexing notification topic"
     message = re.sub('us-west-2', 'af-south-1', message)
     message = re.sub('sentinel-cogs', 'deafrica-sentinel-2', message)
+
     response = sns_hook.publish_to_target(target_arn=default_args['sentinel2_topic_arn'],
                                           message=message, message_attributes=attribute)
 
