a1b6eb6551ac5e0d1f24f207effc348489c7acc8
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-01-18 03:53:41 +0000

    fix typo

--- dags/odc_db_dump.py
+++ dags/odc_db_dump.py
@@ -1,8 +1,8 @@
 from airflow.models import Variable
 
-DB_DUMP_S3_ROLE = Variable.get("db_dump_s3_role", "dea-dev-eks-db-dump-to-s3")
+DB_DUMP_S3_ROLE = Variable.get("db_dump_s3_role", "deafrica-prod-af-eks-db-dump-to-s3")
 SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba-admin")
-DB_DUMP_S3_BUCKET = Variable.get("db_dump_s3_bucketname", "dea-dev-odc-db-dump")
+DB_DUMP_S3_BUCKET = Variable.get("db_dump_s3_bucketname", "deafrica-dev-odc-db-dump")
 
 
 NODE_AFFINITY = {
