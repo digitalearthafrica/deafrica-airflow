00cac0bda398fcf82704a5f7573f87a3ad52a8d7
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-01-19 05:46:17 +0000

    fix typo

--- dags/odc_db_dump.py
+++ dags/odc_db_dump.py
@@ -76,7 +76,7 @@ DUMP_TO_S3_COMMAND = [
         """
             pg_dump -Fc -h $(DB_HOSTNAME) -U $(DB_USERNAME) -d $(DB_DATABASE) > {0}
             ls -la | grep {0}
-            aws s3 cp --acl bucket-owner-full-control {0} s3://{1}/deafrica-prod-af/{0}
+            aws s3 cp --acl bucket-owner-full-control {0} s3://{1}/deafrica-prod/{0}
         """
     ).format(f"odc_{date.today().strftime('%Y_%m_%d')}.pgdump", DB_DUMP_S3_BUCKET),
 ]
