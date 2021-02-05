de4aaf7a323efe479e61d3f13d98293022761752
Mike Mochan <mmochan@mac.com>
2021-02-05 10:55:59 +1000

    update landsat variables

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -1,5 +1,9 @@
 from airflow.models import Variable
 
 SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "landsat_scenes_queue", "deafrica-dev-eks-sync-landsat-scene"
+    "landsat_scenes_sqs_queue", "deafrica-dev-eks-sync-landsat-scene"
+)
+
+SYNC_LANDSAT_CONNECTION_SNS_QUEUE = Variable.get(
+    "landsat_scenes_sns_queue", "deafrica-dev-eks-sync-landsat-sns-scene"
 )
