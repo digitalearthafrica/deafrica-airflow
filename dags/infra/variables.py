7c6ae8026b69c2e0e5db02ca3357e48c6311953f
Mike Mochan <mmochan@mac.com>
2021-02-05 15:31:12 +1000

    Remove sns reference

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -3,7 +3,3 @@ from airflow.models import Variable
 SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
     "landsat_scenes_sqs_queue", "deafrica-dev-eks-sync-landsat-scene"
 )
-
-SYNC_LANDSAT_CONNECTION_SNS_QUEUE = Variable.get(
-    "landsat_scenes_sns_queue", "deafrica-dev-eks-sync-landsat-sns-scene"
-)
