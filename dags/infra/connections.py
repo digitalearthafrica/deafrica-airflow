09dfeb6179c3e80fc4fa5cc5ec10eb7f9ed1bc89
Mike Mochan <mmochan@mac.com>
2021-02-10 13:20:27 +1000

    updates for landsat index

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -1,1 +1,2 @@
 SYNC_LANDSAT_CONNECTION_ID = "sync_landsat_scenes"
+INDEX_LANDSAT_CONNECTION_ID = "index_landsat_scenes"
--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -1,5 +1,9 @@
 from airflow.models import Variable
 
 SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "landsat_scenes_sqs_queue", "deafrica-dev-eks-sync-landsat-scene"
+    "sync_landsat_scenes_sqs_queue", "deafrica-dev-eks-sync-landsat-scene"
+)
+
+INDEX_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
+    "index_landsat_scenes_sqs_queue", "deafrica-dev-eks-index-landsat-scene"
 )
