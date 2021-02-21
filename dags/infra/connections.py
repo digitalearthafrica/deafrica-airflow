ac3067e5dc7f1653f654d6d4eaee90a0873f4bee
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-02-21 23:47:29 +0000

    temp revert to non-ssm

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -1,3 +1,6 @@
-SYNC_LANDSAT_CONNECTION_ID = "sync_landsat_scenes"
+# SYNC_LANDSAT_CONNECTION_ID = "sync_landsat_scenes"
 SYNC_LANDSAT_USGS_CONNECTION_ID = "sync_landsat_scenes_usgs"
 INDEX_LANDSAT_CONNECTION_ID = "index_landsat_scenes"
+
+## reverting to non-ssm
+SYNC_LANDSAT_CONNECTION_ID = "conn_sync_landsat_scene"
