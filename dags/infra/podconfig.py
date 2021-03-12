bd2d315a2e0e796bec63d5031d7997a9e4bcf5fd
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-03-12 03:07:51 +0000

    setup ows dag to use refactored ows

--- dags/infra/podconfig.py
+++ dags/infra/podconfig.py
@@ -21,7 +21,9 @@ NODE_AFFINITY = {
 }
 
 # OWS pod specific configuration
-OWS_CFG_PATH = "/env/config/ows_cfg.py"
+OWS_CFG_FOLDER_PATH = "/env/config/ows_refactored"
 OWS_CFG_MOUNT_PATH = "/env/config"
-OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
-OWS_CFG_IMAGEPATH = "/opt/dea-config/prod/services/wms/ows/ows_cfg.py"
+OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_refactored/dev_af_ows_root_cfg.py"
+OWS_CFG_IMAGEPATH = "/opt/dea-config/services/ows_refactored"
+OWS_PYTHON_PATH = "/env/config"
+OWS_DATACUBE_CFG = "ows_refactored.dev_af_ows_root_cfg.ows_cfg"
