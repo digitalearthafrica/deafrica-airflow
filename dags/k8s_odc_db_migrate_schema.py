fa18f90ad90d3d7da9e099c22115a2a584dbab0b
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-03-26 09:18:39 +1100

    revert previous commit - update schema migration user to explorer-admin again

--- dags/k8s_odc_db_migrate_schema.py
+++ dags/k8s_odc_db_migrate_schema.py
@@ -39,9 +39,9 @@ DEFAULT_ARGS = {
     # Lift secrets into environment variables for datacube database connectivity
     # Use this db-users to run cubedash update-summary
     "secrets": [
-        Secret("env", "DB_DATABASE", "odc-admin", "database-name"),
-        Secret("env", "DB_USERNAME", "odc-admin", "postgres-username"),
-        Secret("env", "DB_PASSWORD", "odc-admin", "postgres-password"),
+        Secret("env", "DB_DATABASE", "explorer-admin", "database-name"),
+        Secret("env", "DB_USERNAME", "explorer-admin", "postgres-username"),
+        Secret("env", "DB_PASSWORD", "explorer-admin", "postgres-password"),
     ],
 }
 
