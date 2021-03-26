f12d08f943ffbfcec4d59026db7ffdcf240b462c
carioca <carioca_rs@yahoo.com.br>
2021-03-26 11:45:38 +1000

    new variables

--- dags/landsat_scenes_sync/variables.py
+++ dags/landsat_scenes_sync/variables.py
@@ -1,7 +1,6 @@
 from infra.variables import (
     LANDSAT_SYNC_S3_BUCKET_NAME,
     AWS_DEFAULT_REGION,
-    AWS_DEFAULT_REGION,
 )
 
 # ######### AWS CONFIG ############
@@ -16,7 +15,7 @@ USGS_AWS_REGION = "us-west-2"
 
 # ######### AFRICA ############
 AFRICA_S3_BUCKET_PATH = f"s3://{LANDSAT_SYNC_S3_BUCKET_NAME}/"
-AFRICA_S3_PRODUCT_EXPLORER = f"https://explorer.digitalearth.africa/products/"
+AFRICA_S3_PRODUCT_EXPLORER = "https://explorer.digitalearth.africa/products/"
 AFRICA_S3_ENDPOINT = "s3.af-south-1.amazonaws.com"
 AFRICA_S3_BUCKET_URL = (
     f"https://{LANDSAT_SYNC_S3_BUCKET_NAME}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/"
