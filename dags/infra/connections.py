542ff1c1f25350db5d2fb9ed9efd22ddd6de945b
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-06 03:17:56 +0000

    add new connecctions

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -1,3 +1,11 @@
 """
 # Connection provided from infra to dags
 """
+INDEX_LANDSAT_CONNECTION_ID = "index_landsat_scenes"
+
+# reverting to non-ssm
+SYNC_LANDSAT_CONNECTION_ID = "conn_sync_landsat_scene"
+
+SYNC_LANDSAT_INVENTORY_ID = "conn_sync_landsat_scene_inventory"
+
+MIGRATION_OREGON_CONNECTION_ID = "deafrica_migration_oregon"
