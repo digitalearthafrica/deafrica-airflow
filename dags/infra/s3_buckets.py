fc96299887c8ed40eda7348a1d9b45ade65ae807
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-11 23:47:47 +0000

    split from variables.py

---
+++ dags/infra/s3_buckets.py
@@ -0,0 +1,28 @@
+"""
+# s3 buckets
+"""
+from airflow.models import Variable
+
+
+SENTINEL_2_INVENTORY_UTILS_BUCKET = Variable.get(
+    "deafrica_sentinel_2", "deafrica-sentinel-2"
+)
+
+SENTINEL_COGS_BUCKET = Variable.get("sentinel_cogs", "sentinel-cogs")
+
+SENTINEL_COGS_INVENTORY_BUCKET = Variable.get(
+    "sentinel_cogs_inventory", "sentinel-cogs-inventory"
+)
+
+SENTINEL_2_INVENTORY_BUCKET = Variable.get(
+    "deafrica_sentinel_2_inventory", "deafrica-sentinel-2-inventory"
+)
+
+
+LANDSAT_SYNC_S3_BUCKET_NAME = Variable.get(
+    "landsat_bucket_name", "deafrica-landsat-dev"
+)
+
+LANDSAT_SYNC_INVENTORY_BUCKET = Variable.get(
+    "deafrica_landsat_inventory", "deafrica-landsat-inventory"
+)
--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -3,46 +3,19 @@
 """
 from airflow.models import Variable
 
-SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "sync_landsat_scenes_sqs_queue", "deafrica-dev-eks-sync-landsat-scene"
-)
-
-INDEX_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "index_landsat_scenes_sqs_queue", "deafrica-dev-eks-index-landsat-scene"
-)
 
 LANDSAT_SYNC_SNS_TOPIC_ARN = Variable.get(
     "landsat_sns_topic",
     "arn:aws:sns:af-south-1:717690029437:deafrica-dev-eks-landsat-topic",
 )
 
-LANDSAT_SYNC_S3_BUCKET_NAME = Variable.get(
-    "landsat_bucket_name", "deafrica-landsat-dev"
-)
-
 LANDSAT_SYNC_S3_C2_FOLDER_NAME = Variable.get("landsat_c2_folder_name", "collection02")
 
-LANDSAT_SYNC_INVENTORY_BUCKET = Variable.get(
-    "deafrica_landsat_inventory", "deafrica-landsat-inventory"
-)
 
 SENTINEL_2_S3_COGS_FOLDER_NAME = Variable.get(
     "deafrica_sentinel_2_cogs_folder", "sentinel-s2-l2a-cogs"
 )
 
-SENTINEL_2_INVENTORY_BUCKET = Variable.get(
-    "deafrica_sentinel_2_inventory", "deafrica-sentinel-2-inventory"
-)
-
-SENTINEL_2_INVENTORY_UTILS_BUCKET = Variable.get(
-    "deafrica_sentinel_2", "deafrica-sentinel-2"
-)
-
-SENTINEL_COGS_BUCKET = Variable.get("sentinel_cogs", "sentinel-cogs")
-
-SENTINEL_COGS_INVENTORY_BUCKET = Variable.get(
-    "sentinel_cogs_inventory", "sentinel-cogs-inventory"
-)
 
 # DB config
 DB_DATABASE = Variable.get("db_database", "odc")
