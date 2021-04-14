7c5c3d8ed4a5d520dceae3c861ba3d9f4293f3d1
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-14 06:19:29 +0000

    update readme

--- dags/infra/podconfig.py
+++ dags/infra/podconfig.py
@@ -22,11 +22,3 @@ ONDEMAND_NODE_AFFINITY = {
         }
     }
 }
-
-# OWS pod specific configuration
-OWS_CFG_FOLDER_PATH = "/env/config/ows_refactored"
-OWS_CFG_MOUNT_PATH = "/env/config"
-OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_refactored/prod_af_ows_root_cfg.py"
-OWS_CFG_IMAGEPATH = "/opt/dea-config/services/ows_refactored"
-OWS_PYTHON_PATH = "/env/config"
-OWS_DATACUBE_CFG = "ows_refactored.prod_af_ows_root_cfg.ows_cfg"
