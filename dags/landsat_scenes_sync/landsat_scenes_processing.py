4af8540b0e36478266644876e84852c94204e04f
carioca <carioca_rs@yahoo.com.br>
2021-04-15 09:24:08 +1000

    added start date

--- dags/landsat_scenes_sync/landsat_scenes_processing.py
+++ dags/landsat_scenes_sync/landsat_scenes_processing.py
@@ -38,9 +38,8 @@ DEFAULT_ARGS = {
     "retries": 0,
     "retry_delay": timedelta(minutes=15),
     "depends_on_past": False,
-    "start_date": datetime.now(),
-    # "start_date": datetime(2021, 3, 29),
-    "catchup": False,
+    "start_date": datetime(2021, 4, 14),
+    "catchup": True,
     "limit_of_processes": 30,
     "version": "0.6.2",
 }
