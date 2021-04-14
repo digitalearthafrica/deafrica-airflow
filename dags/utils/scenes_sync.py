4b86c27e953aaae922215376c98cebdb779a4254
carioca <carioca_rs@yahoo.com.br>
2021-04-14 12:58:54 +1000

    check

--- dags/utils/scenes_sync.py
+++ dags/utils/scenes_sync.py
@@ -5,6 +5,7 @@ import json
 import logging
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from pathlib import Path
 
 from infra.connections import SYNC_LANDSAT_CONNECTION_ID
 from infra.sqs_queues import SYNC_LANDSAT_CONNECTION_SQS_QUEUE
@@ -73,11 +74,14 @@ def request_usgs_api(url: str):
     :return: API return from request_url function
     """ ""
     try:
-        return request_url(url=url)
+        response = request_url(url=url)
+        logging.info(f"stac_version {response.get('stac_version')}")
+        if response.get("stac_version") and response["stac_version"] != "0.7.0":
+            return response
     except Exception as error:
         # If the request return an error, just log and keep going
         logging.error(f"Error requesting API: {error}")
-        return None
+    return None
 
 
 def retrieve_stac_from_api(display_ids):
@@ -91,33 +95,33 @@ def retrieve_stac_from_api(display_ids):
     # Limit number of threads
     num_of_threads = 50
     with ThreadPoolExecutor(max_workers=num_of_threads) as executor:
-        tasks = [
-            executor.submit(
-                request_usgs_api, f"{USGS_API_INDIVIDUAL_ITEM_URL}/{display_id}"
-            )
-            for display_id in display_ids
-        ]
-
         log = False
-        for future in as_completed(tasks):
+        tasks = []
+        for display_id in display_ids:
             if not log:
                 logging.info(f"Requesting USGS API")
                 logging.info(
                     f"Requesting URL {USGS_API_INDIVIDUAL_ITEM_URL} adding the display id by the end of the url"
                 )
-                log = True
+            tasks.append(
+                executor.submit(
+                    request_usgs_api, f"{USGS_API_INDIVIDUAL_ITEM_URL}/{display_id}"
+                )
+            )
+
+        for future in as_completed(tasks):
             if future.result():
                 yield future.result()
 
 
-def filter_africa_location_from_gzip_file(file_path: str, production_date: str):
+def filter_africa_location_from_gzip_file(file_path: Path, production_date: str):
     """
     Function to filter just the Africa location based on the WRS Path and WRS Row. All allowed positions are
     informed through the global variable ALLOWED_PATHROWS which is created when this script file is loaded.
     The function also applies filters to skip LANDSAT_4 and Night shots.
     Warning: This function requires high performance from the CPU.
 
-    :param file_path: (String) Downloaded GZIP file path
+    :param file_path: (Path) Downloaded GZIP file path
     :param production_date: (String) Filter for L2 Product Generation Date ("YYYY/MM/DD" or "YYYY-MM-DD")
     :return: (List) List of Display ids which will be used to retrieve the data from the API.
     """
@@ -125,6 +129,8 @@ def filter_africa_location_from_gzip_file(file_path: str, production_date: str):
 
     # Download updated Pathrows
     africa_pathrows = read_csv_from_gzip(file_path=AFRICA_GZ_PATHROWS_URL)
+
+    # Variable that ensure the log is going to show at the right time
     log = False
     for row in read_big_csv_files_from_gzip(file_path):
         if not log:
@@ -155,70 +161,6 @@ def filter_africa_location_from_gzip_file(file_path: str, production_date: str):
             yield row["Display ID"]
 
 
-# def filter_africa_location_from_gzip_file(file_path: Path):
-#     """
-#     Function to filter just the Africa location based on the WRS Path and WRS Row. All allowed positions are
-#     informed through the global variable ALLOWED_PATHROWS which is created when this script file is loaded.
-#     The function also applies filters to skip LANDSAT_4 and Night shots.
-#     Warning: This function requires high performance from the CPU.
-#
-#     :param file_path: (Path) Downloaded GZIP file path
-#     :return: (List) List of Display ids which will be used to retrieve the data from the API.
-#     """
-#
-#     # Get value of last date from Airflow
-#     saved_last_date = (
-#         convert_str_to_date(Variable.get("landsat_sync_last_date"))
-#         if Variable.get("landsat_sync_last_date", default_var=False)
-#         else ""
-#     )
-#
-#     # Download updated Pathrows
-#     africa_pathrows = read_csv_from_gzip(file_path=AFRICA_GZ_PATHROWS_URL)
-#
-#     logging.info(
-#         f"Unzipping and filtering file according to Africa Pathrows, "
-#         f"day scenes and date {saved_last_date if saved_last_date else ''}"
-#     )
-#
-#     # This variable will update airflow variable,
-#     # in case of by the end of this process the system finds a higher date
-#     last_date = None
-#     for row in read_big_csv_files_from_gzip(file_path):
-#
-#         generated_date = convert_str_to_date(row["Date Product Generated L2"])
-#
-#         # Update last_date to the latest date in the file
-#         if not last_date or generated_date > last_date:
-#             last_date = generated_date
-#             logging.info(
-#                 f"Airflow variable landsat_sync_last_date will be updated to {last_date}"
-#             )
-#
-#         if (
-#             row.get("Satellite")
-#             and row["Satellite"] != "LANDSAT_4"
-#             # Filter to get just day
-#             and (
-#                 row.get("Day/Night Indicator")
-#                 and row["Day/Night Indicator"].upper() == "DAY"
-#             )
-#             # Filter by the generated date comparing to the last Airflow interaction
-#             and (not saved_last_date or saved_last_date < generated_date)
-#             # Filter to get just from Africa
-#             and (
-#                 row.get("WRS Path")
-#                 and row.get("WRS Row")
-#                 and (int(f"{row['WRS Path']}{row['WRS Row']}") in africa_pathrows)
-#             )
-#         ):
-#             yield row["Display ID"]
-#
-#     if not saved_last_date or saved_last_date < last_date:
-#         logging.info(f"Updating Airflow variable to {last_date}")
-#         Variable.set("landsat_sync_last_date", last_date)
-
-
 def sync_data(file_name: str, date_to_process: str):
     """
     Function to initiate the bulk CSV process
@@ -236,29 +178,28 @@ def sync_data(file_name: str, date_to_process: str):
         # Download GZIP file
         file_path = download_file_to_tmp(url=BASE_BULK_CSV_URL, file_name=file_name)
 
-        if file_path:
+        # Read file and retrieve the Display ids
+        display_id_list = filter_africa_location_from_gzip_file(
+            file_path=file_path, production_date=date_to_process
+        )
 
-            # Read file and retrieve the Display ids
-            display_id_list = filter_africa_location_from_gzip_file(
-                file_path=file_path, production_date=date_to_process
-            )
+        if display_id_list:
+            # request the API through the display id and send the information to the queue
+            stac_list = retrieve_stac_from_api(display_ids=display_id_list)
 
-            if display_id_list:
-                # request the API through the display id and send the information to the queue
-                stac_list = retrieve_stac_from_api(display_ids=display_id_list)
+            # Publish stac to the queue
+            # messages_sent = publish_messages(datasets=stac_list)
+            # logging.info(f"Messages sent {messages_sent}")
+            logging.info(f"Messages sent {len([s for s in stac_list])}")
 
-                # Publish stac to the queue
-                messages_sent = publish_messages(datasets=stac_list)
-                logging.info(f"Messages sent {messages_sent}")
-
-            else:
-                logging.info(
-                    f"After filtered no valid or new Ids were found in the file {file_name}"
-                )
-
+        else:
             logging.info(
-                f"File {file_name} processed and sent in {time_process(start=start_timer)}"
+                f"After filtered no valid or new Ids were found in the file {file_name}"
             )
+
+        logging.info(
+            f"File {file_name} processed and sent in {time_process(start=start_timer)}"
+        )
 
         logging.info(f"Whole process finished successfully ;)")
 
