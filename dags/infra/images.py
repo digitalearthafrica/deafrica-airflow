87f899094425986ac9f467f3e6263a47e73e0e35
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-20 03:35:20 +0000

    update explorer image

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -4,7 +4,7 @@
 
 INDEXER_IMAGE = "opendatacube/datacube-index:0.0.16"
 OWS_IMAGE = "opendatacube/ows:1.8.5"
-EXPLORER_IMAGE = "opendatacube/explorer:2.4.0"
+EXPLORER_IMAGE = "opendatacube/explorer:2.5.1"
 OWS_CONFIG_IMAGE = "geoscienceaustralia/deafrica-config:latest"
 
 # UNSTABLE IMAGES
