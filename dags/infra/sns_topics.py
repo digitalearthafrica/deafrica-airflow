bca83854fa91e58e30564dd8f45ce03c5f86a0aa
carioca <carioca_rs@yahoo.com.br>
2021-05-07 15:15:12 +1000

    new variables

---
+++ dags/infra/sns_topics.py
@@ -0,0 +1,14 @@
+"""
+# Variables provided from infra to dags
+"""
+from airflow.models import Variable
+
+LANDSAT_SYNC_SNS_TOPIC_ARN = Variable.get(
+    "landsat_sns_topic",
+    "arn:aws:sqs:af-south-1:565417506782:deafrica-prod-af-eks-index-landsat-scene",
+)
+
+SYNC_SENTINEL_2_CONNECTION_TOPIC_ARN = Variable.get(
+    "sync_sentinel_2_topic_arn",
+    "arn:aws:sns:af-south-1:543785577597:deafrica-sentinel-2-scene-topic",
+)
--- dags/infra/sqs_queues.py
+++ dags/infra/sqs_queues.py
@@ -3,10 +3,24 @@
 """
 from airflow.models import Variable
 
+
 SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
     "sync_landsat_scenes_sqs_queue", "deafrica-prod-af-eks-sync-landsat-scene"
 )
 
+SYNC_LANDSAT_SQS_QUEUE_URL = Variable.get(
+    "sync_landsat_scenes_sqs_queue_url",
+    "https://sqs.af-south-1.amazonaws.com/717690029437/deafrica-dev-eks-sync-landsat-scene",
+)
+
 INDEX_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
     "index_landsat_scenes_sqs_queue", "deafrica-prod-af-eks-index-landsat-scene"
+)
+
+SYNC_SENTINEL_2_CONNECTION_SQS_QUEUE = Variable.get(
+    "sync_sentinel_2_sqs_queue", "deafrica-dev-eks-sentinel-2-sync"
+)
+
+INDEX_SENTINEL_2_CONNECTION_SQS_QUEUE = Variable.get(
+    "index_sentinel_2_scenes_sqs_queue", "deafrica-prod-af-eks-sentinel-2-indexing"
 )
