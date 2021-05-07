481771e1df8da1e3d262c1309e6b385626b403b9
carioca <carioca_rs@yahoo.com.br>
2021-05-07 15:53:39 +1000

    rename SQS and SNS

--- dags/infra/sqs_queues.py
+++ dags/infra/sqs_queues.py
@@ -4,23 +4,23 @@
 from airflow.models import Variable
 
 
-SYNC_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "sync_landsat_scenes_sqs_queue", "deafrica-prod-af-eks-sync-landsat-scene"
+LANDSAT_SYNC_SQS_QUEUE = Variable.get(
+    "landsat_sync_scenes_sqs_queue", "deafrica-prod-af-eks-sync-landsat-scene"
 )
 
-SYNC_LANDSAT_SQS_QUEUE_URL = Variable.get(
-    "sync_landsat_scenes_sqs_queue_url",
+LANDSAT_SYNC_SQS_QUEUE_URL = Variable.get(
+    "landsat_sync_scenes_sqs_queue_url",
     "https://sqs.af-south-1.amazonaws.com/717690029437/deafrica-dev-eks-sync-landsat-scene",
 )
 
-INDEX_LANDSAT_CONNECTION_SQS_QUEUE = Variable.get(
-    "index_landsat_scenes_sqs_queue", "deafrica-prod-af-eks-index-landsat-scene"
+LANDSAT_INDEX_SQS_QUEUE = Variable.get(
+    "landsat_index_scenes_sqs_queue", "deafrica-prod-af-eks-index-landsat-scene"
 )
 
-SYNC_SENTINEL_2_CONNECTION_SQS_QUEUE = Variable.get(
-    "sync_sentinel_2_sqs_queue", "deafrica-dev-eks-sentinel-2-sync"
+SENTINEL_2_SYNC_SQS_QUEUE = Variable.get(
+    "sentinel_2_sync_sqs_queue", "deafrica-dev-eks-sentinel-2-sync"
 )
 
-INDEX_SENTINEL_2_CONNECTION_SQS_QUEUE = Variable.get(
-    "index_sentinel_2_scenes_sqs_queue", "deafrica-prod-af-eks-sentinel-2-indexing"
+SENTINEL_2_INDEX_SQS_QUEUE = Variable.get(
+    "sentinel_2_index_scenes_sqs_queue", "deafrica-prod-af-eks-sentinel-2-indexing"
 )
