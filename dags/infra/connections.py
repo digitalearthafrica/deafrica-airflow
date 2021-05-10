43587940fcdf302fc0f7543ffcacbcad3c16a9ed
carioca <carioca_rs@yahoo.com.br>
2021-05-10 16:32:31 +1000

    new connection names

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -1,12 +1,12 @@
 """
 # Connection provided from infra to dags
 """
-
-
+# ##### LANDSAT #####
 CONN_LANDSAT_SYNC = "conn_landsat_sync"
 
-CONN_LANDSAT_INDEX = "conn_landsat_scenes_index"
+CONN_LANDSAT_INDEX = "conn_landsat_index"
 
-CONN_SENTINEL_2_SYNC = "svc_deafrica_dev_eks_sentinel_2_sync"
+# ##### SENTINEL - 2 #####
+CONN_SENTINEL_2_SYNC = "conn_sentinel_2_sync"
 
 CONN_SENTINEL_2_INDEX = "conn_sentinel_2_index"
