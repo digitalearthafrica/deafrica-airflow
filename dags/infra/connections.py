2ddf436048e7bb9a8434bd819e1f1ec208d72e16
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-05-11 04:54:40 +0000

    infra audit

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -1,12 +1,13 @@
 """
 # Connection provided from infra to dags
+# audit: 11/05/2021
 """
 # ##### LANDSAT #####
-CONN_LANDSAT_SYNC = "conn_landsat_sync"
+CONN_LANDSAT_SYNC = "conn_landsat_sync"  # qa
 
-CONN_LANDSAT_INDEX = "conn_landsat_index"
+CONN_LANDSAT_INDEX = "conn_landsat_index"  # qa
 
 # ##### SENTINEL - 2 #####
-CONN_SENTINEL_2_SYNC = "conn_sentinel_2_sync"
+CONN_SENTINEL_2_SYNC = "conn_sentinel_2_sync"  # qa
 
-CONN_SENTINEL_2_INDEX = "conn_sentinel_2_index"
+CONN_SENTINEL_2_INDEX = "conn_sentinel_2_index"  # qa
