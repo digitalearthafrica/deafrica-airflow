8a23ab07d0a9b237609c561e668bb0c0e0dbfa69
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-05-12 07:05:21 +0000

    update smtp

--- dags/test_smtp.py
+++ dags/test_smtp.py
@@ -1,39 +1,35 @@
+"""
+SMTP infra setup testing dag
+"""
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.email_operator import EmailOperator
 
 
-def print_hello():
-    return "Hello world!"
-
-
 default_args = {
-    "owner": "Pin",
-    "start_date": datetime(2018, 8, 11),
+    "owner": "Pin Jin",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 6, 14),
+    "email": ["pin.jin@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": None,
 }
 
 dag = DAG(
-    "hello_world",
-    description="Simple tutorial DAG",
-    schedule_interval="* * * * *",
+    "test_emailoperator",
+    description="Simple Test DAG",
+    schedule_interval=None,
     default_args=default_args,
     catchup=False,
-)
-
-dummy_operator = DummyOperator(task_id="dummy_task", retries=3, dag=dag)
-
-hello_operator = PythonOperator(
-    task_id="hello_task", python_callable=print_hello, dag=dag
 )
 
 email = EmailOperator(
     task_id="send_email",
     to="pin.jin@ga.gov.au",
     subject="Airflow Alert",
-    html_content=""" <h3>Email Test</h3> """,
+    html_content=None,
     dag=dag,
 )
 
-email >> dummy_operator >> hello_operator
+email
