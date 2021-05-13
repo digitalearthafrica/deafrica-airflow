fe03a3d94a2dae623201f2c179c6ebc884dcd2b9
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-05-13 14:04:14 +1000

    updated email for testing

--- dags/test_smtp.py
+++ dags/test_smtp.py
@@ -10,7 +10,7 @@ default_args = {
     "owner": "Pin Jin",
     "depends_on_past": False,
     "start_date": datetime(2020, 6, 14),
-    "email": ["pin.jin@ga.gov.au"],
+    "email": ["nikita.gandhi@ga.gov.au"],
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": None,
@@ -26,7 +26,7 @@ dag = DAG(
 
 email = EmailOperator(
     task_id="send_email",
-    to="pin.jin@ga.gov.au",
+    to="nikita.gandhi@ga.gov.au",
     subject="Airflow Alert",
     html_content=None,
     dag=dag,
