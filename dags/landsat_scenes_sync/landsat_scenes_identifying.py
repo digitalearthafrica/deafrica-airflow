4c59470d0cf43bd919e1b782fd6b9ff693b352df
carioca <carioca_rs@yahoo.com.br>
2021-05-13 14:58:29 +1000

    logs

--- dags/landsat_scenes_sync/landsat_scenes_identifying.py
+++ dags/landsat_scenes_sync/landsat_scenes_identifying.py
@@ -32,7 +32,7 @@ DEFAULT_ARGS = {
     "retry_delay": timedelta(minutes=15),
     "depends_on_past": False,
     "start_date": datetime(2021, 5, 8),
-    "version": "0.15",
+    "version": "0.16",
 }
 # [END default_args]
 
