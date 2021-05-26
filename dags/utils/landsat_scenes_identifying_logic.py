000ac7a85b63e93c21c201cc647acbadbec64a45
carioca <carioca_rs@yahoo.com.br>
2021-05-26 14:26:33 +1000

    new requirements

--- dags/utils/landsat_scenes_identifying_logic.py
+++ dags/utils/landsat_scenes_identifying_logic.py
@@ -286,7 +286,9 @@ def identifying_data(file_name: str, date_to_process: str):
         if scene_list:
             # request USGS S3 bucket and retrieve list of assets' path
             # TODO remove limitation when in PROD
-            path_list = retrieve_list_of_files(scene_list=[s for s in scene_list][0:8])
+            path_list = retrieve_list_of_files(
+                scene_list=[s for s in scene_list][0:1000]
+            )
             # path_list = retrieve_list_of_files(scene_list=scene_list)
 
             # Publish stac to the queue
