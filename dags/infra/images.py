54160c6a7b471c63f8116dbecfe9656612e810d1
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-05-27 15:22:19 +1000

    update images to use latest

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -2,10 +2,10 @@
 # Images used for KubernetesPodOperator
 """
 
-INDEXER_IMAGE = "opendatacube/datacube-index:0.0.16"
-OWS_IMAGE = "opendatacube/ows:1.8.5"
+INDEXER_IMAGE = "opendatacube/datacube-index:latest"
+OWS_IMAGE = "opendatacube/ows:latest"
 EXPLORER_IMAGE = "opendatacube/explorer:2.5.1"
 OWS_CONFIG_IMAGE = "geoscienceaustralia/deafrica-config:latest"
 
 # UNSTABLE IMAGES
-EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.4.3-73-g5135034"
+EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.5.1-8-gf1c10b0"
