cce484707d3b97c1c0f671b54d7ab4cf03272ce4
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-06-10 16:36:22 +1000

    odc database autovaccum maintenance dag

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -14,16 +14,24 @@ SENTINEL_2_S3_COGS_FOLDER_NAME = Variable.get(
     "deafrica_sentinel_2_cogs_folder", "sentinel-s2-l2a-cogs"
 )
 
-# DB config
-DB_DATABASE = Variable.get("db_database", "odc")
-DB_HOSTNAME = Variable.get("db_hostname", "db-writer")
-
 AWS_DEFAULT_REGION = Variable.get("aws_default_region", "af-south-1")
 SECRET_AWS_NAME = Variable.get("secret_aws_name", "processing-aws-creds-prod")
+
+###### DB config
+DB_DATABASE = Variable.get("db_database", "odc")
+DB_HOSTNAME = Variable.get("db_hostname", "db-writer")
+DB_PORT = Variable.get("db_port", "5432")
+
 SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba-admin")  # qa
 
-SECRET_EXPLORER_WRITER_NAME = Variable.get(
-    "db_explorer_writer_secret", "explorer-writer"
-)
-SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer")
+SECRET_ODC_ADMIN_NAME = Variable.get("db_odc_admin_secret", "odc-admin")
+SECRET_OWS_ADMIN_NAME = Variable.get("db_ows_admin_secret", "ows-admin")
+SECRET_EXPLORER_ADMIN_NAME = Variable.get("db_explorer_admin_secret", "explorer-admin")
+
 SECRET_ODC_WRITER_NAME = Variable.get("db_odc_writer_secret", "odc-writer")
+SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer")
+SECRET_EXPLORER_WRITER_NAME = Variable.get("db_explorer_writer_secret", "explorer-writer")
+
+SECRET_ODC_READER_NAME = Variable.get("db_odc_reader_secret", "odc-reader")
+SECRET_OWS_READER_NAME = Variable.get("db_ows_reader_secret", "ows-reader")
+#####
