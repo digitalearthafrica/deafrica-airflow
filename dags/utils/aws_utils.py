7de0459483db94a37e7b60e4ba70650f770a717b
carioca <carioca_rs@yahoo.com.br>
2021-06-16 12:18:35 +1000

    - change region

--- dags/utils/aws_utils.py
+++ dags/utils/aws_utils.py
@@ -246,7 +246,9 @@ class S3:
         :param request_payer:(str)
         :return:
         """
-        # logging.info(f'get_object {bucket_name} {key} {region} {endpoint_url} {request_payer}')
+        logging.info(
+            f"get_object {bucket_name} {key} {region} {endpoint_url} {request_payer}"
+        )
 
         bucket_client = self.get_bucket_client(region=region, endpoint_url=endpoint_url)
         return bucket_client.get_object(
