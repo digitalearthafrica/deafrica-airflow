a793694e969c60671d5eb802d2a578ea4079f3bf
Rodrigo <carioca_rs@yahoo.com.br>
2021-06-30 11:08:26 +1000

    fixing import

--- dags/generic_indexing.py
+++ dags/generic_indexing.py
@@ -24,7 +24,6 @@ dag_run.conf format:
 """
 import json
 from datetime import datetime, timedelta
-from textwrap import dedent
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
