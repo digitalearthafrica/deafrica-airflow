04c902c0fef6f3596149dfd5d54a7e114496b6dd
Rodrigo <carioca_rs@yahoo.com.br>
2021-07-01 13:06:20 +1000

    test

--- dags/utility/utility_index_from_s3.py
+++ dags/utility/utility_index_from_s3.py
@@ -186,8 +186,8 @@ def indexing_subdag(parent_dag_name, child_dag_name, args, config_task_name):
     try:
         config = json.loads(config)
     except json.decoder.JSONDecodeError:
-        # raise Exception(f'JSON EXCEPTION CONFIG {config} - {type(config)}')
-        config = {}
+        raise Exception(f'JSON EXCEPTION CONFIG {config} - {type(config)}')
+        # config = {}
 
     # logging.info(f"Retrieved Config - {config}")
 
