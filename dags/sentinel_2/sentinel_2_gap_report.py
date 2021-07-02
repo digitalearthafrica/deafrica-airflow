c4d7d45036de21d60f7edf92453abe3fd1702e43
Rodrigo <carioca_rs@yahoo.com.br>
2021-07-02 11:26:20 +1000

    cleaning up

--- dags/sentinel_2/sentinel_2_gap_report.py
+++ dags/sentinel_2/sentinel_2_gap_report.py
@@ -27,7 +27,7 @@ from utils.inventory import InventoryUtils
 from utils.sync_utils import read_csv_from_gzip
 
 default_args = {
-    "owner": "Airflow",
+    "owner": "Rodrigo Carvalho",
     "start_date": datetime(2020, 7, 24),
     "email": ["systems@digitalearthafrica.org"],
     "email_on_failure": True,
