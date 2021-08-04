cdb00f642fa229615b9137c0fd3b7842594e2519
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-08-04 19:19:32 +1000

    adjust slack settings

--- dags/utility/utility_slackoperator.py
+++ dags/utility/utility_slackoperator.py
@@ -1,4 +1,4 @@
-""" slack_operator.py
+""" utility_slackoperator.py
 
 These are alert functions that utilize the Slack operators. Trying from here - https://github.com/tszumowski/airflow_slack_operator
 
@@ -6,8 +6,7 @@ These are alert functions that utilize the Slack operators. Trying from here - h
 from airflow.hooks.base_hook import BaseHook
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
-
-SLACK_CONN_ID = "slack"
+SLACK_CONN_ID = "conn_slack"
 
 
 def task_success_slack_alert(context):
@@ -23,7 +22,7 @@ def task_success_slack_alert(context):
     """
     slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
     slack_msg = """
-            :large_blue_circle: Task Succeeded! 
+            :large_green_circle: [dev airflow] Task Succeeded! 
             *Task*: {task}  
             *Dag*: {dag} 
             *Execution Time*: {exec_date}  
